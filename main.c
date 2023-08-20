21902016a6913d42fb54e0fb202bd95140e421ac
HenriqueMedeirosCosta <132111448+HenriqueMedeirosCosta@users.noreply.github.com>
2023-08-20 18:05:27 -0300

    Add files via upload

--- main.c
+++ main.c
@@ -1,33 +1,56 @@
 #include <stdio.h>
 
-int main(){
-  printf("Cadastro de Usuário\n");
-  char nome[100];
-  int idade[10];
-  char matricula[30];
-  char endereço [50];
+main() {
+
+  char nome[200];
+  int idade;
+  char matricula[20];
+  char endereço[40];
   char curso[40];
-  int período[30];
-  char disciplinas[200];
-  float mensalidade[20];
+  int periodo;
+  char disciplina[100];
+  float mensalidade;
 
+  // segundo passo - entrada de dados
+  printf("Nome: \n");
+  fgets(nome, 200, stdin);
 
- printf("Digite o nome:\n");
-  fgets(nome,100,stdin);  
-  printf("digite sua idade:\n");
-  fgets(idade,10,stdin);
-printf("Digite sua matricula:\n");
-  fgets(matricula,30,stdin);
-printf("Digite seu endereço:\n");
-fgets(endereço,50,stdin);
-printf("Digite seu Curso:\n");
-fgets(curso,40,stdin);
-printf("Digite seu período:\n");
-fgets(período,30,stdin);
-printf("Digite suas disciplinas\n");
-fgets(disciplinas,200,stdin);
-printf("Valor da mensalidade:\n");
-fgets (mensalidade,20,stdin);
+  printf("Idade: \n");
+  scanf("%d",&idade);
+  __fpurge(stdin);
 
-} 
+  printf("Digite a matricula: \n");
+  fgets(matricula, 20, stdin);
+  //__fpurge(stdin);
 
+  printf("Digite o endereço: \n");
+  fgets(endereço, 40, stdin);
+
+  printf("Digite o curso: \n");
+  fgets(curso, 40, stdin);
+
+  printf("Digite o periodo: \n");
+  scanf("%d",&periodo);
+  __fpurge(stdin);
+
+  printf("Digite suas disciplinas: \n");
+  fgets(disciplina, 100, stdin);
+
+  printf("Digite sua mensalidade: \n");
+  scanf("%f",&mensalidade);
+  __fpurge(stdin);
+  
+  //mostram os dados
+
+  printf("Conferência de dados\n");
+
+  printf("Nome:%s",nome);
+  printf("\nIdade:%d",idade);
+  printf("\nMatricula:%s",matricula);
+  printf("\nEndereço:%s",endereço);
+  printf("\nCurso:%s",curso);
+  printf("\nPeriodo:%d",periodo);
+  printf("\nDisciplina:%s",disciplina);
+  printf("\nMensalidade:%.2f",mensalidade);
+  
+}
